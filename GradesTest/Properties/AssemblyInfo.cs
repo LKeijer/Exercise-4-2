efb57674e6ebe47e259a1cef4612d8f3d08853dd
Wafflebasket <luke.keijer@mail.com>
2018-03-01 09:04:15 +0100

    Add project files.

---
+++ GradesTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GradesTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("GradesTest")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e77fceda-2e5d-42d1-bba0-d360e0e503ee")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ GradesTest/UnitTest1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GradesTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        [TestInitialize]
+        public void Init()
+        {
+            GradesPrototype.Data.DataSource.CreateData(); // First add the solution to references in the GradesTest solution. Access it by referring the solutionname you want to use.
+        }
+
+        [TestMethod]
+        public void TestValidGrade()
+        {
+            GradesPrototype.Data.Grade gradetest = new GradesPrototype.Data.Grade(5, "11/5/2000", "History", "B+", "Hitler was not a good person");
+            Assert.AreEqual(gradetest.StudentID, 5);
+            Assert.AreEqual(gradetest.AssessmentDate, "11/05/2000");
+            Assert.AreEqual(gradetest.SubjectName, "History");
+            Assert.AreEqual(gradetest.Assessment, "B+");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestBadDate()
+        {
+            GradesPrototype.Data.Grade wrongdatetest = new GradesPrototype.Data.Grade(3, "1/1/5000", "History", "A", "Aye niggau");
+            
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestDateNotReconized()
+        {
+            GradesPrototype.Data.Grade nonDate = new GradesPrototype.Data.Grade(2, "200/200/200", "Math", "C-", "mapff");
+
+        }
+        
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestBadAssessment()
+        {
+            GradesPrototype.Data.Grade testGrade = new GradesPrototype.Data.Grade();
+            testGrade.Assessment = "T";
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestBadSubject()
+        {
+            GradesPrototype.Data.Grade testSubject = new GradesPrototype.Data.Grade();
+            testSubject.SubjectName = "German";
+        }
+    }
+}
+
